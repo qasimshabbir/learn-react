a68508c01a202c5dd83b50f15d2ce1391ea5cd30
Qasim <qasimshabbir@gmail.com>
2020-06-27 07:58:21 -0500

    Learn about the initial compoanent and props

---
+++ first-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App(props) {
+  return (
+    <div >
+      <h1>Hello {props.name}!</h1> <hd>{5+10}</hd>
+    </div>
+  );
+}
+
+export default App;
