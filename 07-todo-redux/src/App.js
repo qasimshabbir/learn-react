82c71368c11b423e6502520f7b80cfabb26dfd32
Qasim <qasimshabbir@gmail.com>
2021-01-26 17:58:19 +0500

    Todo redux sample app completed

---
+++ 07-todo-redux/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { AddTodo } from './component/AddTodo';
+import { FooterAction } from './component/FooterAction';
+import { ListTodo } from './component/ListTodo';
+
+function App() {
+  return (
+    <div >
+      <ListTodo />
+      <FooterAction />
+      <hr />
+      <AddTodo />
+      
+    </div>
+  );
+}
+
+export default App;
