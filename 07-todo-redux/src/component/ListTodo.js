c09898ffde263dc03beb8a6aa915975902a35dc5
Qasim <qasimshabbir@gmail.com>
2021-01-26 18:50:00 +0500

    added functionality of delete task

--- 07-todo-redux/src/component/ListTodo.js
+++ 07-todo-redux/src/component/ListTodo.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { FILTER_ACTIVE, FILTER_COMPLETED } from '../store/store';
-import { toggleTask } from '../store/todoSlicer';
+import { deleteTask, toggleTask } from '../store/todoSlicer';
 
 export const ListTodo = () => {
 
+    //local state variable
+    const [confirmDelete,setConfirmDelete] = useState(0);
     //getting action from Redux
     const dispatch = useDispatch();
     //getting tasks from Redux
     const tasks= useSelector(state => state.todoReducer.tasks)
     const filter= useSelector(state => state.todoReducer.filter)
+    
 
     //method to apply filter
     const applyFilter = (tasks,filter) => {
@@ -43,6 +46,11 @@ export const ListTodo = () => {
                         />
                         &nbsp;
                         {task.title}
+                        &nbsp;
+                        {task.completed && confirmDelete!==task.id && <button onClick= {(e)=>setConfirmDelete(task.id)}>X</button>}
+                        {confirmDelete===task.id && <button onClick= {(e)=>dispatch(deleteTask(task.id))}>Sure?</button>}
+                        {confirmDelete===task.id && <button onClick= {(e)=>setConfirmDelete(false)}>Cancel</button>}
+                        
                     </li>
                 )
             })}
--- 07-todo-redux/src/store/todoSlicer.js
+++ 07-todo-redux/src/store/todoSlicer.js
@@ -1,5 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const generateUniqueId = ()=>{
+    return Math.floor(Math.random()*100000);
+}
 
 export const todoSlice = createSlice({
     name: 'todoSlice',
@@ -10,22 +13,32 @@ export const todoSlice = createSlice({
     reducers: {
         add: (state,action) => {
             state.tasks.push({
-                id: state.tasks.length,
+                id: generateUniqueId(),
                 title: action.payload,
                 completed: false
             });
         },
         toggleTask: (state,action) => {
+            //This action will mark action as complete and incomplete both.
+            //depending on current state of task.completed
+            
+            //search the task index
             const index = state.tasks.findIndex( todo => todo.id === action.payload);
+            //toggle the state of completed property
             state.tasks[index].completed = !state.tasks[index].completed;
         },
         setFilter: (state,action)=>{
+            //set the filter value from action payload
             state.filter = action.payload;
-        }
+        },
+        deleteTask: (state,action) =>{
+            //delete will setup array without given id in action payload.
+            state.tasks = state.tasks.filter((task)=>task.id !== action.payload);
 
+        }
     }
 });
 
 
-export const {add, toggleTask,setFilter} = todoSlice.actions;
+export const {add, toggleTask,setFilter,deleteTask} = todoSlice.actions;
 export default todoSlice.reducer;
